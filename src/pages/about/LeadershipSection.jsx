c9c61ab29b663f698491308a8d37a4ab24e89c6e
Lino Khan <Lino Khan>
2024-09-18 12:54:51 +0200

    netlify.toml

--- src/pages/about/LeadershipSection.jsx
+++ src/pages/about/LeadershipSection.jsx
@@ -11,8 +11,6 @@ const leaders = [
     { name: 'John Davies', title: 'Chief Executive Officer', imgSrc: Leader_img1, altText: 'Profile picture of John' },
     { name: 'Rob Mayer', title: 'Chief Investment Officer', imgSrc: Leader_img2, altText: 'Profile picture of Rob' },
     { name: 'Peter Jones', title: 'Chief Investment Officer', imgSrc: Leader_img3, altText: 'Profile picture of Peter' },
-   
-
 ];
 
 const LeadershipSection = () => {
